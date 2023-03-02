9f99cecb737dbf1beaeb15d04f9eac0b468e7982
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-02 23:20:40 +0100

    Calculate

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,6 @@
 import styles from './Container.module.scss';
 import React from 'react';
+//import styles from './Button.module.scss';
 
 const Container = props => {
     return(
--- src/components/ShowTime/ShowTime.js
+++ src/components/ShowTime/ShowTime.js
@@ -1,13 +1,66 @@
 import styles from './ShowTime.module.scss';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 
 const ShowTime = () => {
+    const[hour, setHour] = useState(0);
+    const[min, setMin] = useState(0);
+    const[second, setSecond] = useState(0);
+    const[mlSecond, setMilSecond] = useState(0);
+    const[stop, setStop] = useState(true);
 
+    const onStart = () => {
+        setStop(false);
+    }
+    const onStop = () => {
+        setStop(true);
+    }
 
+    const onRest = () => {
+        setMin(0);
+        setHour(0);
+        setMilSecond(0);
+        setSecond(0);
+    }
+
+    useEffect ( () => {
+        let interval = null;
+        if(!stop){
+            interval = setInterval( () => {
+                if(min > 59){
+                    setHour(hour +1);
+                    setMin(0);
+                    clearInterval(interval);
+                }
+                if(second > 59){
+                    setMin(min +1);
+                    setSecond(0);
+                    clearInterval(interval);
+                }
+                if(mlSecond > 999){
+                    setSecond(second +1);
+                    setMilSecond(0);
+                    clearInterval(interval);
+                }
+                if(mlSecond <= 999){
+                    setMilSecond(mlSecond +1);
+                }
+            }, 10)
+        }
+        else{
+            clearInterval(interval);
+        }
+        return () => {
+            clearInterval(interval);
+        }
+    })
     return(
-        <div>
-            <h1 className={styles.text}>Hello</h1>
+        <div className={styles.main}>
+            <h1 className={styles.text}>{hour} : {min} : {second} : {mlSecond}</h1>
+            <button className={styles.start} onClick={onStart}>Start</button>
+            <button className={styles.stop} onClick={onStop}>Stop</button>
+            <button className={styles.rest} onClick={onRest}>Rest</button>
         </div>
     )
 }
